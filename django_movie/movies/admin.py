ef155856c84555c71e1839f2b758d577dd15fc36
Dmytro Kurylko <dimakurylko@gmail.com>
2022-01-11 14:50:51 +0200

    media was added

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -129,7 +129,7 @@ class ActorAdmin(admin.ModelAdmin):
     readonly_fields = ("get_image",)
 
     def get_image(self, obj):
-        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+        return mark_safe(f'<img src={obj.image.url} width="50" height="75"')
 
     get_image.short_description = "Image"
 
@@ -147,7 +147,7 @@ class MovieShotsAdmin(admin.ModelAdmin):
     readonly_fields = ("get_image",)
 
     def get_image(self, obj):
-        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+        return mark_safe(f'<img src={obj.image.url} width="50" height="75"')
 
     get_image.short_description = "Image"
 
